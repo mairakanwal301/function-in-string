6d036a1823397b0184d9d9f188c9b4ecc6b1ec1e
mairarafiq301 <mairarafiq301@gmail.com>
2024-04-18 11:51:38 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,4 @@
+function invite(name) {
+    return "hello , ".concat(name, " you are invited at my birthday.");
+}
+console.log(invite("Maira"));
---
+++ app.ts
@@ -0,0 +1,5 @@
+
+function invite(name:string){
+    return`hello , ${name} you are invited at my birthday.` ;
+}
+console.log(invite("Maira"))
